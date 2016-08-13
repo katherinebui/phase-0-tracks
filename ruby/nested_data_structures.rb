b07f432a505b8f2c83fc2e9f4b73539c7725bc5e
Katherine Bui <kaatherine.buii@gmail.com>
2016-08-12 21:07:25 -0700

    5.5 complete

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,45 @@
+# A classroom full of desks full of supplies 
+
+# build a hash for classroom
+classroom = {
+  # each desk belongs to someone different
+  # name = key
+  
+  Shauna: [
+    "pins", 
+    "sticky notes"
+    ],
+    
+  Norma: {
+    pen: "black",
+    paper: "college ruled",
+    pencil: 7
+  },
+  
+  Deborah: [
+    "pencils", 
+    "staples"
+    ],
+    
+  Maheen: [
+    "scissors", 
+    "sticky notes", 
+    "paper clips"
+    ],
+    
+  Amisha: {
+    folder: "red",
+    highlighter: "yellow"
+  },
+  
+  Joia: "tape"
+}
+
+classroom[:Maheen][1] = "erasers"
+classroom[:Norma].store(:stapler, 20)
+classroom[:Deborah].push("highlighter")
+
+p classroom
+
+classroom.key(7).equal? :pencil
+classroom[:Shauna].length
