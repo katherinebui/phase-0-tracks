337c7dfb5bc1436e18a235c11fd8baa7dddcd5f7
Katherine Bui <kaatherine.buii@gmail.com>
2016-08-15 14:28:28 -0700

    6.1 complete, 6.2 in progress

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+SPECIES ------------------------
+Puppies
+
+CHARACTERISTICS ----------------
+Eyelash length: varies
+Friendly: very
+Eye count: 2
+Name: varies
+Color: varies
+Spotted: varies
+Size: varies
+Age: varies
+
+BEHAVIOR -----------------------
+Jump
+Hug
+Barks
+Walks
+Growls
+
--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,31 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(integer)
+    integer.times do |i|
+    puts "Woof!"
+  end
+  end
+  
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(integer)
+  dog_age = integer * 7
+  puts "The dog is #{dog_age} years old!"
+  end
+
+  def growls
+    puts "Grrrrr!"
+  end
 
 end
 
+Mogley = Puppy.new
+Mogley.fetch("ball")
+Mogley.speak(3)
+Mogley.roll_over
+Mogley.dog_years(2)
+Mogley.growls
