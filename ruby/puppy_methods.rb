8e0ac2ee686e97b6acf24494889587efac932362
Katherine Bui <kaatherine.buii@gmail.com>
2016-08-16 13:34:45 -0700

    changes to puppy methods and new gps 2.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,9 @@
 class Puppy
+  def initialize 
+    puts "Initializing new puppy instance ..."
+  end
+    
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
@@ -26,9 +31,58 @@ class Puppy
 
 end
 
+
 Mogley = Puppy.new
 Mogley.fetch("ball")
 Mogley.speak(3)
 Mogley.roll_over
 Mogley.dog_years(2)
 Mogley.growls
+
+
+
+
+class Baby
+
+  def initialize
+    puts "Initializing new baby instance ..."
+  end
+
+  def cry
+    puts "WAHHHHHHH!"
+  end
+
+  def coo(integer)
+    integer.times do |i|
+    puts "coooooo!"
+  end
+  end
+
+end
+
+Charles = Baby.new
+Charles.cry
+Charles.coo(4)
+
+
+
+baby_list = 50.times.collect{Baby.new}
+def baby_cry(list)
+    list.each do
+    p "hello"
+    end
+end
+    
+baby_cry(baby_list)
+
+baby_list.map!(&:cry)
+
+
+
+
+
+
+
+
+
+
