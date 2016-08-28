25ac1f43fb2180238354323a0ef2541f484aa547
Katherine Bui <kaatherine.buii@gmail.com>
2016-08-27 18:30:57 -0700

    release 0 finall works

--- js/algos.js
+++ js/algos.js
@@ -2,14 +2,26 @@
 // Given array, should be able to pull out the longest string
 // Iterate through the array
 // Use .length to find the longest string
+//    - Need to call .length on all elements
+//    - Pull out the longest out of all
+//    - Takes one initial number and compares until it finds largest
 
 var longest = function(array){
-  for(var i = 0; i < array.length; i++){
-    console.log(array[i])
+  // had to .length -1 because need to include last of the array
+  for(var i = 0; i = array.length - 1; i++){ 
+    var big = array[0]
+    if (big.length < array[i].length){
+      big = array[i]
+      return big
+    }
   }
 };
 
-var array = ["Apple", "Banana", "Orange", "Coconut"];
-longest(array);
+var fruit = ["Kiwi", "Banana", "Orange", "Coconut"];
+console.log(longest(fruit));
+
+var dogNames = ["Mogley", "Speedo", "Chance", "Pippy", "William Wilfred"];
+console.log(longest(dogNames));
+
 
 // Release 1 : 
