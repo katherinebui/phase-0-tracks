275fa848393354209a0eba46b1a690938df6c2e6
Katherine Bui <kaatherine.buii@gmail.com>
2016-08-27 17:17:40 -0700

    iteration over array

---
+++ js/algos.js
@@ -0,0 +1,15 @@
+// Release 0 : Find the Longest Phrase
+// Given array, should be able to pull out the longest string
+// Iterate through the array
+// Use .length to find the longest string
+
+var longest = function(array){
+  for(var i = 0; i < array.length; i++){
+    console.log(array[i])
+  }
+};
+
+var array = ["Apple", "Banana", "Orange", "Coconut"];
+longest(array);
+
+// Release 1 : 
