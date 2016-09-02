0a3b8f2e567b66e494d701006db7cc5a367ee18b
Katherine Bui <kaatherine.buii@gmail.com>
2016-09-02 12:48:22 -0700

    pseudo code bubble sort and edit with the right syntax

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,39 +1,41 @@
 # def search_array(array, number)
-# # checks if the array includes the number 
-#   # index = 0
-#   # while index < array.length
-#   #   if array.include?(number)
-#   #    return array.find_index(number}
-#   # else !array.include?(number)
-#   #    return nil
-#   #   end
-#   # end
-
-# # checks if the array includes the number 
-#   array.each do |element|
-#     if array.include?(number) # check if the number is included in the array
-#       return array.find_index(number) # find the index of number
-#     else !array.include?(number) # if not included return nil
+# checks if the array includes the number 
+#   index = 0
+#   while index < array.length
+#     if array.include?(number)
+#      return array.find_index(number)
+#   else !array.include?(number)
 #      return nil
 #     end
 #   end
 # end
 
+def search_array(array, number)
+# checks if the array includes the number 
+  array.each do |element|
+    if array.include?(number) # check if the number is included in the array
+      return array.find_index(number) # find the index of number
+    else !array.include?(number) # if not included return nil
+     return nil
+    end
+  end
+end
 
-# array = [1, 3, 5, 7, 9, 19, 56]
-# p search_array(array, 0)
-# # return nil
-# p search_array(array, 3)
-# # array[1]
-# # return 1
-# p search_array(array, 19)
-# # return 5
-# p search_array(array, 56)
-# # return 6
-# p search_array(array, 2)
-# # return nil
 
-# # array.at(index#) = returns value at that index position = array.fetch(index#)
+array = [1, 3, 5, 7, 9, 19, 56]
+p search_array(array, 0)
+# return nil
+p search_array(array, 3)
+# array[1]
+# return 1
+p search_array(array, 19)
+# return 5
+p search_array(array, 56)
+# return 6
+p search_array(array, 2)
+# return nil
+
+# array.at(index#) = returns value at that index position = array.fetch(index#)
 
 
 # two arrays
@@ -55,17 +57,17 @@ def fib(number)
   number = number.to_i # take in number and conver it to integer
   array = (0...number).to_a # turn range of the number into array
 
-  new_array = []
-  new_array << array[0]
-  new_array << array[1]
-  array.delete(0)
+  new_array = [] # initialize new array
+  new_array << array[0] # pushing the first number into the array
+  new_array << array[1] # pushing the second number to array to have two beginning #
+  array.delete(0) # delete the number from the first array
 
   # DRY - loop through because repeat in code
-  index = 0 
+  index = 0  # start loop to run thru the whole array
   while index < array.length - 1 # doesn't want to include the last sum
     sum = new_array[-1] + new_array[-2] # index positions of the last two #
-    new_array << sum
-    index += 1
+    new_array << sum # pushing the new number into the new array
+    index += 1 # no infinite loops please
   end
     
   p new_array
@@ -78,19 +80,35 @@ fib(20)
 fib(100)
 
 
+# SORTING THE ARRAY : BUBBLE SORT
+# https://www.sitepoint.com/sorting-algorithms-ruby/
+# http://stackoverflow.com/questions/23504909/bubble-sorting-logic-in-ruby-method
+# https://www.youtube.com/watch?v=x9G7_1RvyB8
 
+def bubble_sort(array) # pass an array = list of numbers
+  n = array.length # set as a variable so easier to read later, cs convention n = array, total of array
+  loop do # start the loop to check each value of array and compares them
+    swapped = false # check to see if the item is false, looking for break out of loop
+    (n-1).times do |i| # want one less than 1 array, in the list of #, want to access on # at a time = |i|
+      if array[i] > array[i+1] # the position of the number in the list
+    # since this is conditional, it will only run if the first part is true
+    # array[i] = refers to the position of the number in the list
+        array[i], array[i+1] = array[i+1], array[i] # if the condition is true, the numbers will swap the two positions
+        swapped = true 
+      end
+    end
+    break if not swapped # break out of the loop only if swap is not true
+  end
+  array # want to return the array
+end
 
+array = [4, 5, 2, 5, 6, 8, 3]
+p bubble_sort(array)
 
+a = [1, 2, 4, 6, 3, 7, 4, 9]
+p bubble_sort(a)
 
-
-
-
-
-
-
-
-
-
+ 
 
 
 
