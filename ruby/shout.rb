12bc01eed53be10f0bba1e88fd259e2c87a5d933
Katherine Bui <kaatherine.buii@gmail.com>
2016-09-07 12:04:53 -0700

    add code to shout module and loaded it

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,8 +1,10 @@
 # please ignore everything below the line. I am re-doing the assignment for practice!
-module Shout
-  # we'll put some methods here soon, but this code is fine for now!
-end
 
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+end
 
 
 
@@ -53,26 +55,26 @@ end
 # end
 
 
-module Shout
-  def scream(words)
-    puts "#{words}! WHAT DO YOU WANT FROM ME?!"
-  end
-end
+# module Shout
+#   def scream(words)
+#     puts "#{words}! WHAT DO YOU WANT FROM ME?!"
+#   end
+# end
 
-class Human
-  include Shout
-end
+# class Human
+#   include Shout
+# end
 
-class Kitten
-  include Shout
-end
+# class Kitten
+#   include Shout
+# end
 
-man = Human.new
-p "HEY MAN!"
-man.scream("AHHHHHHHHHHHH!!!!!!")
+# man = Human.new
+# p "HEY MAN!"
+# man.scream("AHHHHHHHHHHHH!!!!!!")
 
-karl = Kitten.new
-p "HEY KARL!"
-karl.scream("Grrrrrrr")
+# karl = Kitten.new
+# p "HEY KARL!"
+# karl.scream("Grrrrrrr")
 
 
