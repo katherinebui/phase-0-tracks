92697fa4dc92805829569acd30eb31f130a2c83b
Katherine Bui <kaatherine.buii@gmail.com>
2016-09-30 15:22:58 -0700

    after thestarting over for the third time, we finished

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,10 +7,22 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+tipsy = SQLite3::Database.new("tips.db")
+tipsy.results_as_hash = true
+
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
   erb :home
+end
+
+get '/feedback' do
+  @tips = tipsy.execute("SELECT * FROM tips")
+  erb :feedback
+end
+
+get '/tips/new' do
+  erb :anon_form
 end
 
 get '/students/new' do
@@ -23,5 +35,12 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+post '/tips' do
+  tipsy.execute("INSERT INTO tips (tip) VALUES (?)", [params['tip']])
+  redirect '/feedback'
+end
+
+
 
 # add static resources
